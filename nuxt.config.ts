ead721d7669394350dff72a7b92210fa4f9d02ac
Martin Kocisek <kocisek.martin@gmail.com>
2025-11-17 07:25:03 +0100

    feat: multiple emails

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,6 +73,8 @@ export default defineNuxtConfig({
     smtpFromEmail: process.env.SMTP_FROM_EMAIL,
     smtpFromName: process.env.SMTP_FROM_NAME || 'LevFood',
     smtpSecure: process.env.SMTP_SECURE || 'false',
-    adminNotificationEmail: process.env.ADMIN_NOTIFICATION_EMAIL || 'kocisek.martin@gmail.com',
+    adminNotificationEmails: process.env.ADMIN_NOTIFICATION_EMAIL
+      ? process.env.ADMIN_NOTIFICATION_EMAIL.split(',').map(email => email.trim())
+      : ['kocisek.martin@gmail.com'],
   }
 })
